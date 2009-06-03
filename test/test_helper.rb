8f8c37741bb70e0c531ef4df4b80b4d42f828e61
Markus Prinz <markus.prinz@nuclearsquid.com>
2009-06-03 14:25:39 +0200

    Update test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,15 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require 'rr'
+
+# Try to load it, but don't care if it fails
+require 'redgreen' rescue nil
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'long_url'
 
 class Test::Unit::TestCase
+  include RR::Adapters::TestUnit
 end
